caf80eb513ede3d16ad5214f97f945f8cc14d156
Mastermindddddd <malusicephas17@gmail.com>
2023-07-27 15:53:29 +0200

    commit no 17

--- server/index.js
+++ server/index.js
@@ -7,11 +7,13 @@ const cors = require('cors')
 const bcrypt = require('bcrypt')
 require('dotenv').config()
 
-const uri = process.env.URI
+
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+
+const uri = process.env.URI
 
 // Default
 app.get('/', (req, res) => {
