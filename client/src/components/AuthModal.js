53058f7c755311080f6e2b3b8c50d3c2442c226d
Mastermindddddd <malusicephas17@gmail.com>
2023-08-06 21:05:31 +0200

    commit 58

--- client/src/components/AuthModal.js
+++ client/src/components/AuthModal.js
@@ -45,7 +45,7 @@ const AuthModal = ({ setShowModal, isSignUp }) => {
         return;
       }
 
-      const response = await axios.post(`https://kiomate-server.vercel.app/${isSignUp ? 'signup' : 'login'}`, { email, password });
+      const response = await axios.post(`https://dark-ruby-mackerel-gown.cyclic.app/${isSignUp ? 'signup' : 'login'}`, { email, password });
 
       setCookie('AuthToken', response.data.token);
       setCookie('UserId', response.data.userId);
