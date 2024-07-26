a59f96484887e4a2dee707f45fa02a9c42795fcd
hikenrides <cephas@hikenrides.com>
2024-07-27 00:17:39 +0200

    ...

--- server/allowCors.js
+++ server/allowCors.js
@@ -1,6 +1,6 @@
 const allowCors = fn => async (req, res) => {
     res.setHeader('Access-Control-Allow-Credentials', true)
-    res.setHeader('Access-Control-Allow-Origin', 'https://kiomate.online', 'http://localhost:3000')
+    res.setHeader('Access-Control-Allow-Origin', '*')
     // another common pattern
     // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
     res.setHeader('Access-Control-Allow-Methods', '*')
