a8512bd351c214a62ee6d2c9876f48d64bb73e2d
hikenrides <cephas@hikenrides.com>
2024-08-25 00:15:55 +0200

    cors error debugging

--- server/index.js
+++ server/index.js
@@ -1,16 +1,16 @@
-const PORT = process.env.PORT || 8000
-const express = require('express')
-const {MongoClient} = require('mongodb')
-const {v4: uuidv4} = require('uuid')
-const jwt = require('jsonwebtoken')
-const cors = require('cors')
-const bcrypt = require('bcrypt')
-require('dotenv').config()
+const PORT = process.env.PORT || 8000;
+const express = require('express');
+const {MongoClient} = require('mongodb');
+const {v4: uuidv4} = require('uuid');
+const jwt = require('jsonwebtoken');
+const cors = require('cors');
+const bcrypt = require('bcrypt');
+require('dotenv').config();
 
 
 const uri = process.env.URI
 const corsOptions = {
-    origin: ['https://kiomate.online', 'http://localhost:5173', 'http://localhost:5174'],
+    origin: ['https://kiomate.online', 'http://localhost:3000'],
     credentials: true,
     exposedHeaders: ['set-cookie'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
